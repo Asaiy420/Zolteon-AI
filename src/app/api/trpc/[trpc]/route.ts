4f45d370d52c6fab41bb7b2b4133337593b8a9a0
Asaiy420 <kcaryan05@gmail.com>
2025-06-30 10:43:33 +0545

    add: better comments

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -6,6 +6,6 @@ const handler = (req: Request) =>
     endpoint: '/api/trpc',
     req,
     router: appRouter,
-    createContext: createTRPCContext,
+    createContext: createTRPCContext, // creates a context for each request
   });
 export { handler as GET, handler as POST };
