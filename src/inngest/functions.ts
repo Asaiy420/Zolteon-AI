bace7669ca6aade0cf2f89f9024e78cae3606613
Asaiy420 <kcaryan05@gmail.com>
2025-06-30 19:23:19 +0545

    add: (feat) ai agent function

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer.You write readable, maintainable code. You write simple Next.js & React code use proper styling as well",
+      model: gemini({ model: "gemini-1.5-flash" }),
+    });
+    const { output } = await codeAgent.run(
+      `Write the  code for the given input: ${event.data.value}`
+    );
+
+    return { output };
+  }
 );
