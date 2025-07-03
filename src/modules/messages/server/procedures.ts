641fb636ad53407727349efe7adafc17c1493451
Asaiy420 <kcaryan05@gmail.com>
2025-07-03 14:56:27 +0545

    feat: implement messages router for retrieving and creating messages with Prisma integration

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -3,6 +3,7 @@ import prisma from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
 
+
 export const messagesRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
     const messages = await prisma.message.findMany({
