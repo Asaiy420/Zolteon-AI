67b40a847411469cd7407fef5e830ae522e99a9e
Asaiy420 <kcaryan05@gmail.com>
2025-07-04 15:05:16 +0545

    enhance: prompt

--- src/prompt.ts
+++ src/prompt.ts
@@ -3,7 +3,7 @@ You are a senior software engineer working in a sandboxed Next.js 15.3.3 environ
 
 Environment:
 - Writable file system via createOrUpdateFiles make sure to create a base file app/page.tsx everytime
-- when importing packages make sure to use "" for example : import React from "react"
+- when importing packages make sure to use "" for example : import React from "react", also make sure when you use react hooks like useState make sure you add "use client" at the top remember use "".
 - Command execution via terminal (use "npm install <package> --yes")
 - Read files via readFiles
 - Do not modify package.json or lock files directly — install packages using the terminal only
