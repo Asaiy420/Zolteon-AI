f3f68b9ce64af436a5db7fe26a9ea1bd446eb1ef
Asaiy420 <kcaryan05@gmail.com>
2025-07-05 11:26:00 +0545

    feat: add getOne procedure to projects router for retrieving a single project by ID with error handling

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -3,8 +3,34 @@ import prisma from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
 import { generateSlug } from "random-word-slugs";
+import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
+  getOne: baseProcedure
+
+    .input(
+      z.object({
+        id: z.string().min(1, { message: "ID is required" }),
+      })
+    )
+
+    .query(async ({ input }) => {
+      const existingProjects = await prisma.project.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!existingProjects) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: `Project with ID ${input.id} not found`,
+        });
+      }
+
+      return existingProjects;
+    }),
+
   getMany: baseProcedure.query(async () => {
     const projects = await prisma.project.findMany({
       orderBy: {
@@ -16,12 +42,9 @@ export const projectsRouter = createTRPCRouter({
   create: baseProcedure
     .input(
       z.object({
-        value: z
-          .string()
-          .min(1, { message: "Prompt is required" })
-          .max(10000, {
-            message: "Prompt must be less than 10,000 characters",
-          }),
+        value: z.string().min(1, { message: "Prompt is required" }).max(10000, {
+          message: "Prompt must be less than 10,000 characters",
+        }),
       })
     )
     .mutation(async ({ input }) => {
