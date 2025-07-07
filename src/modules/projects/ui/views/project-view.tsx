20972291d2a3ac27f397daf5ea6f161a72dea70b
Asaiy420 <kcaryan05@gmail.com>
2025-07-07 11:56:23 +0545

    feat: enhance ProjectView to include ProjectHeader and manage active fragments in MessagesContainer

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -6,13 +6,16 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
   projectId: string;
 }
 
 export const ProjectView = ({ projectId }: Props) => {
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
   return (
     <div className="h-screen flex flex-col">
       <ResizablePanelGroup direction="horizontal">
@@ -21,8 +24,15 @@ export const ProjectView = ({ projectId }: Props) => {
           minSize={20}
           className="flex flex-col h-full min-h-0"
         >
+          <Suspense fallback={<p>Loading project header...</p>}>
+            <ProjectHeader projectId={projectId} />
+          </Suspense>
           <Suspense fallback={<p>Loading messages...</p>}>
-            <MessagesContainer projectId={projectId} />
+            <MessagesContainer
+              projectId={projectId}
+              activeFragment={activeFragment}
+              setActiveFragment={setActiveFragment}
+            />
           </Suspense>
         </ResizablePanel>
         <ResizableHandle withHandle />
