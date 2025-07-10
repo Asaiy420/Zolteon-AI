23a95698da4167f62ca306a51c07fe3e8991f870
Asaiy420 <kcaryan05@gmail.com>
2025-07-10 14:21:52 +0545

    feat: display user's first name in Previous Projects header and conditionally render component based on user authentication

--- src/modules/home/ui/components/projects-list.tsx
+++ src/modules/home/ui/components/projects-list.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
+import { useUser } from "@clerk/nextjs";
 import { useQuery } from "@tanstack/react-query";
 import { formatDistanceToNow } from "date-fns";
 import Image from "next/image";
@@ -9,11 +10,16 @@ import Link from "next/link";
 
 export const PorjectsList = () => {
   const trpc = useTRPC();
+  const { user } = useUser();
   const { data: projects } = useQuery(trpc.projects.getMany.queryOptions());
+
+  if (!user) return null;
 
   return (
     <div className="full-width bg-white dark:bg-sidebar rounded-xl p-8 border flex flex-col gap-y-6 sm:gap-y-4">
-      <h2 className="text-2xl font-semibold">Previous Projects</h2>
+      <h2 className="text-2xl font-semibold">
+        {user?.firstName}&apos;s Previous Projects
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
         {projects?.length === 0 && (
           <div className="col-span-full text-center ">
